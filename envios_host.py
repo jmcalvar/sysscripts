a9a4cd8dc5b9f6430757e3ecf2305eb9e65ab53f
jmcalvar <jmcalvar@gmail.com>
2017-04-18 15:55:24 +0200

    Update envios_host.py

--- envios_host.py
+++ envios_host.py
@@ -1,20 +1,11 @@
-"""Script para envio de ficheros desde el servidor de storage a caixabank
-para su tratamiento"""
-
-
-# Importamos los modulos necesarios
-# Como el script esta en desarrollo, algunos modulos
-# no son necesarios todavia aunque estan incluidos
-
-import sqlite3 as lite
-import sys
 import glob
 import os
 import time
 from datetime import datetime,timedelta
 from shutil import copyfile
 import shutil
-import archivar
+import paramiko
+import archivar # Este modulo lo creamos nosotros para simplificar el archivado
 
 # Directorio donde se encuentran los videos
 ORIGEN="/backup/video/"
@@ -47,89 +38,103 @@ NOMBREFICH="BOB20001."
 
 def InsertarBBDD(fichero, ficheronuevo ):
   con = lite.connect('envios.db')
- con = lite.connect('envios.db')
   with con:
     cur = con.cursor()
     sql = "INSERT INTO Registro (Fich, FichN, Mydate) VALUES (?, ?, ?)"
     cur.execute(sql, (fichero,ficheronuevo,str(datetime.now())))
 
+def ChequeoBBDD():
+  con = lite.connect('envios.db')
+  with con:
+    cur = con.cursor()
+    cur.execute( 'SELECT  Fich FROM Registro' )
+    rows = cur.fetchall()
+return rows
 
+def CreacionDirectorio():
+  """Chequeamos si existe el directorio y si no existe se crea"""
+  if not os.path.exists(NOMBREPAQUETE):
+    os.makedirs(NOMBREPAQUETE)
 
-
-
-def buscar5Ficheros():
+def buscarFicheros():
   """Funcion de busqueda de ficheros y declaracion de una lista"""
   lista=[]
   files=glob.glob(ORIGEN+"*.mp4")
   files.sort(key=os.path.getmtime)
   return files
 
-
-def CopiarFicheros():
- """Funcion para copiar los ficheros con el nuevo nombre en el directorio
- e incluirlos en el zip correspondiente"""
- lista5=[]
- lista=buscar5Ficheros()
- if lista:
-   lista5=lista[-5:]
-   if not os.path.exists(NOMBREPAQUETE):
-     os.makedirs(NOMBREPAQUETE)
-     index=0
-     for i in lista5:
-       index += 1
-       shutil.copyfile(i,str(NOMBREPAQUETE)+"/"+str(NOMBREPAQUETE)+"_"+str(FECHAFICH)+"00000"+str(index)+".MP4")
-       InsertarBBDD(i,str(NOMBREPAQUETE)+"_"+str(FECHAFICH)+"00000"+str(index)+".MP4")
-
-   shutil.make_archive(NOMBREPAQUETE, 'zip',NOMBREPAQUETE)
- else:
-   print "no hay ficheros"
 def FicherosFiltrados():
   # Si esta variable es NO, se incluye todo
   # Si esta variable es "SI", se filtra lo que metemos en la listsa
-  FILTRAR="NO"
+  FILTRAR="SI"
   index=0
   # Creamos una lista con todos los ficheros
-  lista=buscar5Ficheros()
+  lista=buscarFicheros()
 
   # Creamos una lista vacia
   lista_filtrada=[]
 
   # lista de 5 en caso necesario
-  lista5=lista[-5:]
-  if not os.path.exists(NOMBREPAQUETE):
-    os.makedirs(NOMBREPAQUETE)
+  lista5=lista[-10:]
+
+  # Chequeamos si existe el directorio y si no es asi lo creamos
+
+  creacion = CreacionDirectorio()
 
   NOMBRENUEVO=str(NOMBREPAQUETE)+"_"+str(FECHAFICH)+"00000"+str(index)+".MP4"
   # Como filtro tenemos el dia de ayer
   FILTRO=str(FECHAAYER.year)+str(FECHAAYER.month)+str(FECHAAYER.day)
+
+  # Cuando dejemos de hacer demos en lugar de utilizar
+  # lista5 utilizaremos lista con todo el contenido
   for i in lista5:
     fech_epoc=os.path.getmtime(i)
     fecha=datetime.fromtimestamp(fech_epoc)
     sinfiltro=str(fecha.year)+str(fecha.month)+str(fecha.day)
-
-    # Si el hay ficheros de ayer se incluyen en la lista
-    if sinfiltro == FILTRO:
-      lista_filtrada.append(i)
-      index += 1
-      shutil.copyfile(i,str(NOMBREPAQUETE)+"/"+str(NOMBREPAQUETE)+"_"+str(FECHAFICH)+"00000"+str(index)+".MP4")
-      InsertarBBDD(i,str(NOMBREPAQUETE)+"_"+str(FECHAFICH)+"00000"+str(index)+".MP4")
-    # Si no coincide el filtro ahora elegimos si metemos todo o no
-    elif FILTRAR == "SI":
+    print fecha
+    print sinfiltro
+    print FILTRO
+    if FILTRAR == "NO":
       print "no guardo nada"
       break
-    else:
+
+    # Si hay ficheros de ayer los incluiremos en la lista si no hay no incluira nada
+    elif sinfiltro == FILTRO:
+      registros = ChequeoBBDD()
+      for registro in registros:
+        lista_filtrada.append(i)
+        index += 1
+        shutil.copyfile(i,str(NOMBREPAQUETE)+"/"+str(NOMBREPAQUETE)+"_"+str(FECHAFICH)+"00000"+str(index)+".MP4")
+        InsertarBBDD(i,str(NOMBREPAQUETE)+"_"+str(FECHAFICH)+"00000"+str(index)+".MP4")
+
+
+    # Si no coincide el filtro ahora elegimos si metemos todo o no
+    #else:
       # Metemos todo efectivamente
-      #lista_filtrada.append(i)
-      #InsertarBBDD(i,i)
       # Hasta aqui si metemos todo lo anterior
-      index += 1
-      shutil.copyfile(i,str(NOMBREPAQUETE)+"/"+str(NOMBREPAQUETE)+"_"+str(FECHAFICH)+"00000"+str(index)+".MP4")
-      InsertarBBDD(i,str(NOMBREPAQUETE)+"_"+str(FECHAFICH)+"00000"+str(index)+".MP4")
+    #  index += 1
+    #  shutil.copyfile(i,str(NOMBREPAQUETE)+"/"+str(NOMBREPAQUETE)+"_"+str(FECHAFICH)+"00000"+str(index)+".MP4")
+    #  InsertarBBDD(i,str(NOMBREPAQUETE)+"_"+str(FECHAFICH)+"00000"+str(index)+".MP4")
 
     shutil.make_archive(NOMBREPAQUETE, 'zip',NOMBREPAQUETE)
 
-
   return  lista
 
-#a=CopiarFicheros()
-a = FicherosFiltrados()
+def RevisionTotal():
+  pass
+
+def conexionSFTP():
+  """ Para la conexion por sftp utilizamos paramiko
+  client = paramiko.SSHClient()
+  client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+  # Logicamente falta incluir la ip y el fichero pem
+  client.connect(<IP Address>, port=22, username=<User Name>, key_filename=<.PEM File path)
+  # Setup sftp connection and transmit this script
+  #print "copying"
+  sftp = client.open_sftp()
+  sftp.put(<Source>, <Destination>)
+  sftp.close()"""
+  pass
+
+#a = FicherosFiltrados()
+print b
